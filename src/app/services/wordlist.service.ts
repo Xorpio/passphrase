a65d1300ef1497b6726d0715fd6c88903be60b5b
Gooijer <niek.de.gooijer@centric.eu>
2019-03-15 11:13:06 +0100

    better logic and implementation of the wordListService

--- src/app/services/wordlist.service.ts
+++ src/app/services/wordlist.service.ts
@@ -1,41 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class WordlistService {
 
-  private dutchList = './assets/DicewareDutch.json';
-  private englishList = './assets/diceware.wordlist.json';
+  private defaultLang = 'EN';
+  private languageDict = { 'EN': './assets/diceware.wordlist.json', 'NL': './assets/DicewareDutch.json' };
 
   constructor(private http: HttpClient) { }
 
   GetList(language: string): Observable<any> {
-
-    // logic to check local storage
-    switch (language) {
-      case 'EN':
-        return this.getEnglishList();
-      case 'NL':
-        return this.getDutchList();
-    }
+    return this.http.get(this.languageDict[language] || this.languageDict[this.defaultLang]);
   }
-
-  logError(error: any): any {
-    console.log(error);
-  }
-  log(data: any): any {
-    console.log(data);
-  }
-
-  getDutchList(): Observable<any> {
-    return this.http.get(this.dutchList);
-  }
-
-  getEnglishList(): Observable<any> {
-    return this.http.get(this.englishList);
-  }
-
 }
